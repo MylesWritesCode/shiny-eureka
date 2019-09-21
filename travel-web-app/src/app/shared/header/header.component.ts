fb6d16bca6bb974b968ff7bd80397a86c6594f4e
@myles.berueda <MylesWritesCode@gmail.com>
2019-09-20 23:27:06 -0700

    finished basic header

---
+++ travel-web-app/src/app/shared/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [
+    './header.component.scss',
+    '../../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
