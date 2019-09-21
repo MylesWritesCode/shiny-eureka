98fca33806ebddeadc0ac0d9bbaa9be10e68cc27
@myles.berueda <MylesWritesCode@gmail.com>
2019-09-21 01:47:01 -0700

    initial enhancements

--- travel-web-app/src/app/app-routing.module.ts
+++ travel-web-app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopFiveComponent } from './pages/top-five/top-five.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TopFiveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- travel-web-app/src/app/app.module.ts
+++ travel-web-app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from '@app/shared/shared.module';
+import { TopFiveComponent } from './pages/top-five/top-five.component';
 
 
 @NgModule({
-  declarations: [ AppComponent, ],
+  declarations: [ AppComponent, TopFiveComponent, ],
   imports: [
     BrowserModule,
     AppRoutingModule,
